44b42fb8b358502070fc4d5489b296dbebae192d
Bkhomola <bkhomola@gmail.com>
2023-04-02 07:34:53 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-void before_main() __attribute__((constructor));
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void before_main(void)
 
 {
 	printf(You're beat! and yet, you must allow,\nI bore my house upon my back!)\n");
