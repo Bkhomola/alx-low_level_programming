a0c611afbd9803ac91854f45b430028a57c9ce3f
Bkhomola <bkhomola@gmail.com>
2023-04-16 03:01:03 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,32 @@
  */
 
 int create_file(const char *filename, char *text_content)
+{
+	int len;
+	int fd, wr;
 
-{	/* Pointer to the file */
-	FILE *fp1;
-	/* Character variable to read the content of file */
-	char c;
+	if (filename == NULL)
+		return (-1);
 
-	/* Opening a file in r mode*/
-	fp1 = fopen("C:\\myfiles\\newfile.txt", "r");
-
-	/* Infinite loop –I have used break to come out of the loop*/
-	while (1)
+	if (text_content == NULL)
 	{
-	c = fgetc(fp1);
-	if (c == EOF)
-	break;
-	else
-		printf("%c", c);
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
 	}
-	fclose(fp1);
-	return (0);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	for (len = 0; text_content[len]; len++)
+	{}
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
