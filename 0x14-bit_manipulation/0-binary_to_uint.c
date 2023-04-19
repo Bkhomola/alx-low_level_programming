13c5afc4a9fb904c124a25b72193f612f8bbe624
Bkhomola <bkhomola@gmail.com>
2023-04-19 09:19:46 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,18 @@
 unsigned int binary_to_uint(const char *b)
 
 {
-	unsigned int k = 2;
-	unsigned int i;
-	unsigned int c;
-	unsigned int len;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	len = strlen(b);
-	for (c = len; c > 0; c--)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[c] != 48 || b[c] != 49)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		if (b[c] == '1')
-		{
-			i += atoi(b) * k;
-		}
-		k *= 2;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (i);
+
+	return (dec_val);
 }
